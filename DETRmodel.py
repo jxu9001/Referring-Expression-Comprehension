34d4d32d91f4c2965d6f1293d3912ff16fa2e35e
Nithin Pingili <nithinp300@gmail.com>
2023-04-10 17:58:40 -0500

    added util files

--- DETRmodel.py
+++ DETRmodel.py
@@ -7,13 +7,6 @@ from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
                        accuracy, get_world_size, interpolate,
                        is_dist_avail_and_initialized)
 
-from .backbone import build_backbone
-from .matcher import build_matcher
-from .segmentation import (DETRsegm, PostProcessPanoptic, PostProcessSegm,
-                           dice_loss, sigmoid_focal_loss)
-from .transformer import build_transformer
-
-
 class DETR(nn.Module):
     """ This is the DETR module that performs object detection """
     def __init__(self, backbone, transformer, num_classes, num_queries, aux_loss=False):
